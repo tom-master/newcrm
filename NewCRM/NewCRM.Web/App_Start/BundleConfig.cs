3a1ed5d454bc67290ee6f065ab4634ea312a3996
haozifan <haozifan@outlook.com>
2015-08-01 11:51:49 +0800

    [

--- NewCRM/NewCRM.Web/App_Start/BundleConfig.cs
+++ NewCRM/NewCRM.Web/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace NewCRM.Web
 {
@@ -10,7 +9,7 @@ namespace NewCRM.Web
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
-
+            
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
