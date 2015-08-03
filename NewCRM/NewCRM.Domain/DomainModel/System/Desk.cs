03f5edeeaf7ddc9b279a1e66ec7ed91471ada9f5
haozifan <haozifan@outlook.com>
2015-08-03 16:14:55 +0800

    1

---
+++ NewCRM/NewCRM.Domain/DomainModel/System/Desk.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NewCRM.Domain.DomainModel.Account;
+
+namespace NewCRM.Domain.DomainModel.System
+{
+    public class Desk : EntityBase<Int32>
+    {
+        private Int32 _userId;
+
+        private String _app;
+
+        private User _user;
+
+
+        public Desk()
+        {
+        }
+
+        public Int32 UserId
+        {
+            get
+            {
+                return _userId;
+            }
+            set { _userId = value; }
+        }
+
+        public String App
+        {
+            get { return _app; }
+            set { _app = value; }
+        }
+
+
+        public virtual User User
+        {
+            get { return _user; }
+            set { _user = value; }
+        }
+
+
+    }
+}
