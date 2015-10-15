e48ab01d8eea765aff864773d83a4c543caf7cdc
haozifan <haozifan@outlook.com>
2015-10-15 08:35:52 +0800

    1

--- NewCRM/NewCRM.Domain/DomainModel/System/UserConfig.cs
+++ NewCRM/NewCRM.Domain/DomainModel/System/UserConfig.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NewCRM.Domain.DomainModel.Account;
 
 namespace NewCRM.Domain.DomainModel.System
