94f76d5671d234588c659636e0aaa18cf89c96bd
haozifan <haozifan@outlook.com>
2016-03-03 16:59:14 +0800

    ww

---
+++ NewCRM/NewCRM.Infrastructure/Repositories/Repositories/Account/Impl/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NewCRM.Domain.DomainModel.Account;
+using NewCRM.Infrastructure.Repositories.RepositoryProvide;
+
+namespace NewCRM.Infrastructure.Repositories.Repositories.Account.Impl
+{
+    internal class UserRepository : EfRepositoryBase<User, Int32>
+    {
+        public override void Add(User entity, bool isSave = true)
+        {
+            base.Add(entity, isSave);
+        }
+
+        public override void Remove(IEnumerable<User> entities, bool isSave = true)
+        {
+            base.Remove(entities, isSave);
+        }
+
+        public override void Update(User entity, bool isSave = true)
+        {
+            base.Update(entity, isSave);
+        }
+
+        public override IQueryable<User> Entities { get; }
+    }
+}
---
+++ NewCRM/NewCRM.Infrastructure/Repositories/Repositories/Security/Impl/PowerRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NewCRM.Domain.DomainModel.Security;
+using NewCRM.Infrastructure.Repositories.RepositoryProvide;
+
+namespace NewCRM.Infrastructure.Repositories.Repositories.Security.Impl
+{
+    internal class PowerRepository : EfRepositoryBase<Power, Int32>
+    {
+        public override void Add(Power entity, bool isSave = true)
+        {
+            base.Add(entity, isSave);
+        }
+
+        public override void Remove(IEnumerable<Power> entities, bool isSave = true)
+        {
+            base.Remove(entities, isSave);
+        }
+
+        public override void Update(Power entity, bool isSave = true)
+        {
+            base.Update(entity, isSave);
+        }
+
+        public override IQueryable<Power> Entities { get; }
+    }
+}
