ce22c1772f9832126b29a4d7d08d1fd1c99918cf
haozifan <haozifan@outlook.com>
2017-08-20 14:44:39 +0800

    提交更新

--- NewCRM/NewCRM.Domain/Factory/DomainQuery/ConcreteQuery/DefaultQuery.cs
+++ NewCRM/NewCRM.Domain/Factory/DomainQuery/ConcreteQuery/DefaultQuery.cs
@@ -12,102 +12,137 @@ using NewCRM.Infrastructure.CommonTools.CustomExtension;
 
 namespace NewCRM.Domain.Factory.DomainQuery.ConcreteQuery
 {
-    [Export("EF", typeof(QueryBase))]
-    internal class DefaultQuery : QueryBase
-    {
-        private readonly IDomainModelQueryProvider _queryProvider;
+	[Export("EF", typeof(QueryBase))]
+	internal class DefaultQuery : QueryBase
+	{
+		private readonly IDomainModelQueryProvider _queryProvider;
 
-        private readonly DomainFactory _domainFactory;
+		private readonly DomainFactory _domainFactory;
 
-        [ImportingConstructor]
-        public DefaultQuery(
-            [Import("EF", typeof(IDomainModelQueryProvider))]IDomainModelQueryProvider queryProvider,
-            [Import(typeof(DomainFactory))] DomainFactory domainFactory)
-        {
-            _queryProvider = queryProvider;
+		[ImportingConstructor]
+		public DefaultQuery(
+			[Import("EF", typeof(IDomainModelQueryProvider))]IDomainModelQueryProvider queryProvider,
+			[Import(typeof(DomainFactory))] DomainFactory domainFactory)
+		{
+			_queryProvider = queryProvider;
 
-            _domainFactory = domainFactory;
-        }
+			_domainFactory = domainFactory;
+		}
 
-        /// <summary>
-        /// 查找并返回单个对象
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="specification"></param>
-        /// <param name="selector"></param>
-        /// <returns></returns>
-        public override T FindOne<T>(Specification<T> specification, Expression<Func<T, Object>> selector)
-        {
-            return ConvertToDomain<T>(_queryProvider.Query(specification).Select(selector).ToList()).FirstOrDefault();
-        }
+		/// <summary>
+		/// 查找并返回单个对象
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="specification"></param>
+		/// <param name="selector"></param>
+		/// <returns></returns>
+		public override T FindOne<T>(Specification<T> specification, Expression<Func<T, Object>> selector)
+		{
+			return ConvertToDomain<T>(_queryProvider.Query(specification).Select(selector).ToList()).FirstOrDefault();
+		}
 
-        /// <summary>
-        /// 查找并返回集合
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="specification"></param>
-        /// <param name="selector"></param>
-        /// <returns></returns>
-        public override IEnumerable<T> Find<T>(Specification<T> specification, Expression<Func<T, Object>> selector)
-        {
-            return ConvertToDomain<T>(_queryProvider.Query(specification).Select(selector).ToList());
-        }
+		/// <summary>查找并返回单个对象</summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="specification"></param>
+		/// <returns></returns>
+		public override T FindOne<T>(Specification<T> specification)
+		{
+			return _queryProvider.Query(specification).FirstOrDefault();
+		}
 
-        /// <summary>
-        /// 分页
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="specification"></param>
-        /// <param name="selector"></param>
-        /// <param name="pageIndex"></param>
-        /// <param name="pageSize"></param>
-        /// <param name="totalCount"></param>
-        /// <returns></returns>
-        public override IEnumerable<T> PageBy<T>(Specification<T> specification, Int32 pageIndex, Int32 pageSize, out Int32 totalCount, Expression<Func<T, Object>> selector)
-        {
-
-            IQueryable<T> query = _queryProvider.Query(specification);
-
-            totalCount = query.Count();
-
-            return ConvertToDomain<T>(query.PageBy(pageIndex, pageSize, specification.OrderBy).Select(selector).ToList());
-
-        }
+		/// <summary>
+		/// 查找并返回集合
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="specification"></param>
+		/// <param name="selector"></param>
+		/// <returns></returns>
+		public override IEnumerable<T> Find<T>(Specification<T> specification, Expression<Func<T, Object>> selector)
+		{
+			return ConvertToDomain<T>(_queryProvider.Query(specification).Select(selector).ToList());
+		}
 
 
-        public IEnumerable<T> ConvertToDomain<T>(IEnumerable<Object> parameters) where T : DomainModelBase, IAggregationRoot
-        {
-            if (!parameters.Any())
-            {
-                return new List<T>();
-            }
+		/// <summary>查找并返回集合</summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="specification"></param>
+		/// <returns></returns>
+		public override IEnumerable<T> Find<T>(Specification<T> specification)
+		{
+			return _queryProvider.Query(specification);
+		}
 
-            var parameterType = parameters.FirstOrDefault().GetType();
+		/// <summary>
+		/// 分页
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="specification"></param>
+		/// <param name="selector"></param>
+		/// <param name="pageIndex"></param>
+		/// <param name="pageSize"></param>
+		/// <param name="totalCount"></param>
+		/// <returns></returns>
+		public override IEnumerable<T> PageBy<T>(Specification<T> specification, Int32 pageIndex, Int32 pageSize, out Int32 totalCount, Expression<Func<T, Object>> selector)
+		{
 
-            var domainType = typeof(T);
+			IQueryable<T> query = _queryProvider.Query(specification);
 
-            IList<T> lst = new List<T>();
+			totalCount = query.Count();
 
-            foreach (var parameter in parameters)
-            {
-                var instance = _domainFactory.Create<T>();
+			return ConvertToDomain<T>(query.PageBy(pageIndex, pageSize, specification.OrderBy).Select(selector).ToList());
 
-                lst.Add(instance);
+		}
 
-                foreach (var domainProperty in domainType.GetProperties())
-                {
-                    var property = parameterType.GetProperty(domainProperty.Name);
+		/// <summary>分页</summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="specification"></param>
+		/// <param name="pageIndex"></param>
+		/// <param name="pageSize"></param>
+		/// <param name="totalCount"></param>
+		/// <returns></returns>
+		public override IEnumerable<T> PageBy<T>(Specification<T> specification, int pageIndex, int pageSize, out int totalCount)
+		{
+			IQueryable<T> query = _queryProvider.Query(specification);
 
-                    if (property == null)
-                    {
-                        continue;
-                    }
+			totalCount = query.Count();
 
-                    domainProperty.SetValue(instance, property.GetValue(parameter));
-                }
-            }
+			return query.PageBy(pageIndex, pageSize, specification.OrderBy);
+		}
 
-            return lst;
-        }
-    }
+
+		public IEnumerable<T> ConvertToDomain<T>(IEnumerable<Object> parameters) where T : DomainModelBase, IAggregationRoot
+		{
+			if (!parameters.Any())
+			{
+				return new List<T>();
+			}
+
+			var parameterType = parameters.FirstOrDefault().GetType();
+
+			var domainType = typeof(T);
+
+			IList<T> lst = new List<T>();
+
+			foreach (var parameter in parameters)
+			{
+				var instance = _domainFactory.Create<T>();
+
+				lst.Add(instance);
+
+				foreach (var domainProperty in domainType.GetProperties())
+				{
+					var property = parameterType.GetProperty(domainProperty.Name);
+
+					if (property == null)
+					{
+						continue;
+					}
+
+					domainProperty.SetValue(instance, property.GetValue(parameter));
+				}
+			}
+
+			return lst;
+		}
+	}
 }
--- NewCRM/NewCRM.Web/Filter/ErrorFilter.cs
+++ NewCRM/NewCRM.Web/Filter/ErrorFilter.cs
@@ -15,35 +15,35 @@ namespace NewCRM.Web.Filter
         {
             filterContext.ExceptionHandled = true;
 
-            DependencyResolver.Current.GetService<ILoggerApplicationServices>().AddLogger(new LogDto
-            {
-                Action = filterContext.RouteData.Values["action"].ToString(),
-                Controller = filterContext.RouteData.Values["controller"].ToString(),
-                ExceptionMessage = filterContext.Exception.Message.Length > 20 ? "操作失败，请查看日志" : filterContext.Exception.Message,
-                Track = filterContext.Exception.StackTrace,
-                LogLevelEnum = 4,
-                Id = new Random().Next(1, Int32.MaxValue),
-                AddTime = DateTime.Now.ToString(CultureInfo.CurrentCulture)
-            });
+            //DependencyResolver.Current.GetService<ILoggerApplicationServices>().AddLogger(new LogDto
+            //{
+            //    Action = filterContext.RouteData.Values["action"].ToString(),
+            //    Controller = filterContext.RouteData.Values["controller"].ToString(),
+            //    ExceptionMessage = filterContext.Exception.Message.Length > 20 ? "操作失败，请查看日志" : filterContext.Exception.Message,
+            //    Track = filterContext.Exception.StackTrace,
+            //    LogLevelEnum = 4,
+            //    Id = new Random().Next(1, Int32.MaxValue),
+            //    AddTime = DateTime.Now.ToString(CultureInfo.CurrentCulture)
+            //});
 
-            var errorMessage = filterContext.Exception.GetType() == typeof(BusinessException) ? filterContext.Exception.Message : "操作失败，请查看日志";
+            //var errorMessage = filterContext.Exception.GetType() == typeof(BusinessException) ? filterContext.Exception.Message : "操作失败，请查看日志";
 
-            if (filterContext.RequestContext.HttpContext.Request.HttpMethod.ToLower() != "post")
-            {
-                filterContext.Result = new ContentResult
-                {
-                    Content = @"<script>setTimeout(function(){window.top.ZENG.msgbox.show('" + errorMessage + "', 5,3000);},0)</script>",
-                    ContentEncoding = Encoding.UTF8
-                };
-            }
-            else
-            {
-                filterContext.Result = new JsonResult
-                {
-                    ContentEncoding = Encoding.UTF8,
-                    Data = new { js = @"<script>setTimeout(function(){window.top.ZENG.msgbox.show('" + errorMessage + "', 5,3000);},0)</script>" }
-                };
-            }
+            //if (filterContext.RequestContext.HttpContext.Request.HttpMethod.ToLower() != "post")
+            //{
+            //    filterContext.Result = new ContentResult
+            //    {
+            //        Content = @"<script>setTimeout(function(){window.top.ZENG.msgbox.show('" + errorMessage + "', 5,3000);},0)</script>",
+            //        ContentEncoding = Encoding.UTF8
+            //    };
+            //}
+            //else
+            //{
+            //    filterContext.Result = new JsonResult
+            //    {
+            //        ContentEncoding = Encoding.UTF8,
+            //        Data = new { js = @"<script>setTimeout(function(){window.top.ZENG.msgbox.show('" + errorMessage + "', 5,3000);},0)</script>" }
+            //    };
+            //}
         }
     }
 }
