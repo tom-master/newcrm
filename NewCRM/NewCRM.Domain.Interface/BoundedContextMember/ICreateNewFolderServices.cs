79b55e9509dc607ea26e998b78c0447cb8c53f83
haozifan <haozifan@outlook.com>
2017-10-02 11:11:16 +0800

    物理删除多余的接口层

--- NewCRM/NewCRM.Domain.Interface/BoundedContextMember/ICreateNewFolderServices.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace NewCRM.Domain.Interface.BoundedContextMember
-{
-    public interface ICreateNewFolderServices
-    {
-        void NewFolder(Int32 deskId, String folderName, String folderImg);
-    }
-}
