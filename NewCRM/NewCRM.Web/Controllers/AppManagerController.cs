3ed536a55e6b8f1b995e9a3c5cbfbfbcf6d423f2
haozifan <haozifan@outlook.com>
2017-10-05 12:43:17 +0800

    修改app管理action的返回值

--- NewCRM/NewCRM.Web/Controllers/AppManagerController.cs
+++ NewCRM/NewCRM.Web/Controllers/AppManagerController.cs
@@ -1,136 +1,166 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 using NewCRM.Application.Services.Interface;
 using NewCRM.Dto.Dto;
+using NewCRM.Infrastructure.CommonTools;
 using NewCRM.Web.Controllers.ControllerHelper;
 
 namespace NewCRM.Web.Controllers
 {
-    public class AppManagerController : BaseController
-    {
+	public class AppManagerController : BaseController
+	{
 
-        private readonly IAppApplicationServices _appApplicationServices;
-
-        
-        public AppManagerController(IAppApplicationServices appApplicationServices)
-        {
-            _appApplicationServices = appApplicationServices;
-        }
+		private readonly IAppApplicationServices _appApplicationServices;
 
 
-        #region 页面
-
-        // GET: AppManage
-        public ActionResult Index()
-        {
-            ViewData["AppTypes"] = _appApplicationServices.GetAppTypes();
-
-            ViewData["AppStyles"] = _appApplicationServices.GetAllAppStyles().ToList();
-
-            ViewData["AppStates"] = _appApplicationServices.GetAllAppStates().Where(w => w.Name == "未审核" || w.Name == "已发布").ToList();
-
-            return View();
-        }
+		public AppManagerController(IAppApplicationServices appApplicationServices)
+		{
+			_appApplicationServices = appApplicationServices;
+		}
 
 
-        public ActionResult AppAudit(Int32 appId)
-        {
-            AppDto appResult = null;
-            if (appId != 0)// 如果appId为0则是新创建app
-            {
-                appResult = _appApplicationServices.GetApp(appId);
+		#region 页面
 
-                ViewData["AppState"] = appResult.AppAuditState;
-            }
+		/// <summary>
+		/// 首页
+		/// </summary>
+		/// <returns></returns>
+		public ActionResult Index()
+		{
+			ViewData["AppTypes"] = _appApplicationServices.GetAppTypes();
 
-            ViewData["AppTypes"] = _appApplicationServices.GetAppTypes();
+			ViewData["AppStyles"] = _appApplicationServices.GetAllAppStyles().ToList();
 
-            return View(appResult);
-        }
+			ViewData["AppStates"] = _appApplicationServices.GetAllAppStates().Where(w => w.Name == "未审核" || w.Name == "已发布").ToList();
 
-        #endregion
+			return View();
+		}
 
-        /// <summary>
-        /// 获取所有的app
-        /// </summary>
-        /// <param name="searchText"></param>
-        /// <param name="appTypeId"></param>
-        /// <param name="appStyleId"></param>
-        /// <param name="appState"></param>
-        /// <param name="pageIndex"></param>
-        /// <param name="pageSize"></param>
-        /// <returns></returns>
-        public ActionResult GetAllApps(String searchText, Int32 appTypeId, Int32 appStyleId, String appState, Int32 pageIndex, Int32 pageSize)
-        {
-            Int32 totalCount;
+		/// <summary>
+		/// app审核
+		/// </summary>
+		/// <param name="appId"></param>
+		/// <returns></returns>
+		public ActionResult AppAudit(Int32 appId)
+		{
+			AppDto appResult = null;
+			if (appId != 0)// 如果appId为0则是新创建app
+			{
+				appResult = _appApplicationServices.GetApp(appId);
 
-            var appResults = _appApplicationServices.GetAccountAllApps(0,searchText, appTypeId, appStyleId, appState, pageIndex, pageSize, out totalCount);
+				ViewData["AppState"] = appResult.AppAuditState;
+			}
 
-            return Json(new
-            {
-                apps = appResults,
-                totalCount
-            }, JsonRequestBehavior.AllowGet);
-        }
+			ViewData["AppTypes"] = _appApplicationServices.GetAppTypes();
 
-        /// <summary>
-        /// app审核通过
-        /// </summary>
-        /// <param name="appId"></param>
-        /// <returns></returns>
-        public ActionResult Pass(Int32 appId)
-        {
-            _appApplicationServices.Pass(appId);
+			return View(appResult);
+		}
 
-            return Json(new
-            {
-                success = 1
-            });
-        }
+		#endregion
 
-        /// <summary>
-        /// app审核不通过
-        /// </summary>
-        /// <param name="appId"></param>
-        /// <returns></returns>
-        public ActionResult Deny(Int32 appId)
-        {
-            _appApplicationServices.Deny(appId);
+		/// <summary>
+		/// 获取所有的app
+		/// </summary>
+		/// <returns></returns>
+		public ActionResult GetAllApps(Int32 accountId, String searchText, Int32 appTypeId, Int32 appStyleId, String appState, Int32 pageIndex, Int32 pageSize)
+		{
+			var response = new ResponseModels<IList<AppDto>>();
+			Int32 totalCount;
+			var result = _appApplicationServices.GetAccountAllApps(accountId, searchText, appTypeId, appStyleId, appState, pageIndex, pageSize, out totalCount);
+			if (result != null)
+			{
+				response.TotalCount = totalCount;
+				response.IsSuccess = true;
+				response.Message = "获取app列表成功";
+				response.Model = result;
+			}
+			else
+			{
+				response.Message = "获取app列表失败";
+			}
+			return Json(response, JsonRequestBehavior.AllowGet);
+		}
 
-            return Json(
-                new
-                {
-                    success = 1
-                });
-        }
+		/// <summary>
+		/// app审核通过
+		/// </summary>
+		/// <param name="appId"></param>
+		/// <returns></returns>
+		public ActionResult Pass(Int32 appId)
+		{
+			#region 参数验证	
 
-        /// <summary>
-        /// 设置app为今日推荐
-        /// </summary>
-        /// <param name="appId"></param>
-        /// <returns></returns>
-        public ActionResult Recommend(Int32 appId)
-        {
-            _appApplicationServices.SetTodayRecommandApp(appId);
+			Parameter.Validate(appId);
 
-            return Json(
-                new
-                {
-                    success = 1
-                });
-        }
+			#endregion
 
-        /// <summary>
-        /// 删除app
-        /// </summary>
-        /// <param name="appId"></param>
-        /// <returns></returns>
-        public ActionResult RemoveApp(Int32 appId)
-        {
-            _appApplicationServices.RemoveApp(appId);
+			var response = new ResponseModel();
+			_appApplicationServices.Pass(appId);
 
-            return Json(new { sucess = 1 });
-        }
-    }
+			response.IsSuccess = true;
+			response.Message = "app审核通过";
+			return Json(response);
+		}
+
+		/// <summary>
+		/// app审核不通过
+		/// </summary>
+		/// <param name="appId"></param>
+		/// <returns></returns>
+		public ActionResult Deny(Int32 appId)
+		{
+			#region 参数验证	
+
+			Parameter.Validate(appId);
+
+			#endregion
+
+			var response = new ResponseModel();
+			_appApplicationServices.Deny(appId);
+
+			response.IsSuccess = true;
+			response.Message = "app审核不通过";
+			return Json(response);
+		}
+
+		/// <summary>
+		/// 设置app为今日推荐
+		/// </summary>
+		/// <param name="appId"></param>
+		/// <returns></returns>
+		public ActionResult Recommend(Int32 appId)
+		{
+			#region 参数验证	
+			Parameter.Validate(appId);
+			#endregion
+
+			var response = new ResponseModel();
+			_appApplicationServices.SetTodayRecommandApp(appId);
+
+			response.IsSuccess = true;
+			response.Message = "设置成功";
+			return Json(response);
+		}
+
+		/// <summary>
+		/// 删除app
+		/// </summary>
+		/// <param name="appId"></param>
+		/// <returns></returns>
+		public ActionResult RemoveApp(Int32 appId)
+		{
+			#region 参数验证	
+			Parameter.Validate(appId);
+			#endregion
+
+			var response = new ResponseModel();
+			_appApplicationServices.RemoveApp(appId);
+
+			response.IsSuccess = true;
+			response.Message = "删除app成功";
+			return Json(response);
+		}
+	}
 }
