55466f3ccf1b3eca9a91f8740038625140fa7a28
haoxiaofan <569192072@qq.com>
2017-10-06 12:23:13 +0800

    LoginController修改过长的变量名

--- NewCRM/NewCRM.Web/Controllers/LoginController.cs
+++ NewCRM/NewCRM.Web/Controllers/LoginController.cs
@@ -10,15 +10,14 @@ using Newtonsoft.Json;
 
 namespace NewCRM.Web.Controllers
 {
-	public class LoginController : BaseController
-	{
-		private readonly IAccountApplicationServices _accountApplicationServices;
+    public class LoginController : BaseController
+    {
+        private readonly IAccountApplicationServices _accountServices;
 
-
-		public LoginController(IAccountApplicationServices accountApplicationServices)
-		{
-			_accountApplicationServices = accountApplicationServices;
-		}
+        public LoginController(IAccountApplicationServices accountServices)
+        {
+            _accountServices = accountServices;
+        }
 
         #region 页面
         /// <summary>
@@ -26,9 +25,9 @@ namespace NewCRM.Web.Controllers
         /// </summary>
         /// <returns></returns>
         public ActionResult Index()
-		{
-			return View();
-		}
+        {
+            return View();
+        }
 
         #endregion
 
@@ -36,30 +35,30 @@ namespace NewCRM.Web.Controllers
         /// 登陆
         /// </summary>
         /// <returns></returns>
-        public ActionResult Landing(String accountName, String passWord, Boolean isRememberPasswrod = false)
-		{
-			var response = new ResponseModel<AccountDto>();
+        public ActionResult Landing(String accountName, String passWord, Boolean isRememberPasswrod)
+        {
+            var response = new ResponseModel<AccountDto>();
 
-			#region 参数验证
-			Parameter.Validate(accountName).Validate(passWord);
-			#endregion
+            #region 参数验证
+            Parameter.Validate(accountName).Validate(passWord);
+            #endregion
 
-			var account = _accountApplicationServices.Login(accountName, passWord);
-			if (account != null)
-			{
-				response.Message = "登陆成功";
-				response.Model = account;
-				response.IsSuccess = true;
+            var account = _accountServices.Login(accountName, passWord);
+            if (account != null)
+            {
+                response.Message = "登陆成功";
+                response.Model = account;
+                response.IsSuccess = true;
 
-				Response.SetCookie(new HttpCookie("Account")
-				{
-					Name = account.Id.ToString(),
-					Value = JsonConvert.SerializeObject(account),
-					Expires = isRememberPasswrod ? DateTime.Now.AddDays(7) : DateTime.Now.AddMinutes(30)
-				});
-			}
+                Response.SetCookie(new HttpCookie("Account")
+                {
+                    Name = account.Id.ToString(),
+                    Value = JsonConvert.SerializeObject(account),
+                    Expires = isRememberPasswrod ? DateTime.Now.AddDays(7) : DateTime.Now.AddMinutes(30)
+                });
+            }
 
-			return Json(response);
-		}
-	}
+            return Json(response);
+        }
+    }
 }
