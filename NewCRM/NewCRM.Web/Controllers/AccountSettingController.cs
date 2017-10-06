c3a06f9507088aba57763d3af4ffe5fe241cc8f7
haoxiaofan <569192072@qq.com>
2017-10-06 12:09:06 +0800

    AppManagerController修改过长的变量名

--- NewCRM/NewCRM.Web/Controllers/AccountSettingController.cs
+++ NewCRM/NewCRM.Web/Controllers/AccountSettingController.cs
@@ -7,124 +7,119 @@ using NewCRM.Web.Controllers.ControllerHelper;
 
 namespace NewCRM.Web.Controllers
 {
-	public class AccountSettingController : BaseController
-	{
-		private readonly IAccountApplicationServices _accountServices;
+    public class AccountSettingController : BaseController
+    {
+        private readonly IAccountApplicationServices _accountServices;
 
-		public AccountSettingController(IAccountApplicationServices accountApplicationServices)
-		{
-			_accountServices = accountApplicationServices;
-		}
+        public AccountSettingController(IAccountApplicationServices accountServices)
+        {
+            _accountServices = accountServices;
+        }
 
-		#region 页面
-		/// <summary>
-		/// 首页
-		/// </summary>
-		/// <param name="accountId"></param>
-		/// <returns></returns>
-		public ActionResult Index(Int32 accountId)
-		{
-			return View(_accountServices.GetAccount(accountId));
-		}
+        #region 页面
+        /// <summary>
+        /// 首页
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index(Int32 accountId)
+        {
+            return View(_accountServices.GetAccount(accountId));
+        }
 
-		#endregion
+        #endregion
 
-		/// <summary>
-		///上传账户头像
-		/// </summary>
-		/// <returns></returns>
-		public ActionResult UploadFace(Int32 accountId)
-		{
+        /// <summary>
+        ///上传账户头像
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult UploadFace(Int32 accountId)
+        {
             #region 参数验证
             Parameter.Validate(accountId);
             #endregion
 
             var response = new ResponseModel<dynamic>();
-			if (Request.Files.Count != 0)
-			{
-				var icon = Request.Files[0];
+            if (Request.Files.Count != 0)
+            {
+                var icon = Request.Files[0];
 
-				var fileUpLoadHelper = new FileUpLoadHelper(ConfigurationManager.AppSettings["UploadIconPath"], false, true);
-				if (fileUpLoadHelper.SaveFile(icon))
-				{
-					_accountServices.ModifyAccountFace(accountId, fileUpLoadHelper.FilePath + fileUpLoadHelper.NewFileName);
+                var fileUpLoadHelper = new FileUpLoadHelper(ConfigurationManager.AppSettings["UploadIconPath"], false, true);
+                if (fileUpLoadHelper.SaveFile(icon))
+                {
+                    _accountServices.ModifyAccountFace(accountId, fileUpLoadHelper.FilePath + fileUpLoadHelper.NewFileName);
 
-					response.Message = "头像上传成功";
-					response.IsSuccess = true;
-				}
-				else
-				{
-					response.Message = "头像上传失败";
-				}
-			}
-			else
-			{
-				response.Message = "请选择一张图片后再进行上传";
-			}
-			return Json(response);
-		}
+                    response.Message = "头像上传成功";
+                    response.IsSuccess = true;
+                }
+                else
+                {
+                    response.Message = "头像上传失败";
+                }
+            }
+            else
+            {
+                response.Message = "请选择一张图片后再进行上传";
+            }
+            return Json(response);
+        }
 
-		/// <summary>
-		/// 修改账户登陆密码
-		/// </summary>
-		/// <param name="forms"></param>
-		/// <returns></returns>
-		public ActionResult ModifyAccountPassword(FormCollection forms)
-		{
-			#region 参数验证
-			Parameter.Validate(forms);
-			#endregion
+        /// <summary>
+        /// 修改账户登陆密码
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ModifyAccountPassword(FormCollection forms)
+        {
+            #region 参数验证
+            Parameter.Validate(forms);
+            #endregion
 
-			var response = new ResponseModel();
-			_accountServices.ModifyPassword(Int32.Parse(forms["accountId"]), forms["password"]);
-			response.Message = "账户密码修改成功";
-			response.IsSuccess = true;
+            var response = new ResponseModel();
+            _accountServices.ModifyPassword(Int32.Parse(forms["accountId"]), forms["password"]);
+            response.Message = "账户密码修改成功";
+            response.IsSuccess = true;
 
-			return Json(response);
-		}
+            return Json(response);
+        }
 
-		/// <summary>
-		/// 修改锁屏密码
-		/// </summary>
-		/// <param name="forms"></param>
-		/// <returns></returns>
-		public ActionResult ModifyLockScreenPassword(FormCollection forms)
-		{
-			#region 参数验证
+        /// <summary>
+        /// 修改锁屏密码
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ModifyLockScreenPassword(FormCollection forms)
+        {
+            #region 参数验证
 
-			Parameter.Validate(forms);
+            Parameter.Validate(forms);
 
-			#endregion
+            #endregion
 
-			var response = new ResponseModel();
-			_accountServices.ModifyLockScreenPassword(Int32.Parse(forms["accountId"]), forms["lockpassword"]);
+            var response = new ResponseModel();
+            _accountServices.ModifyLockScreenPassword(Int32.Parse(forms["accountId"]), forms["lockpassword"]);
 
-			response.Message = "锁屏密码修改成功";
-			response.IsSuccess = true;
+            response.Message = "锁屏密码修改成功";
+            response.IsSuccess = true;
 
-			return Json(response);
-		}
+            return Json(response);
+        }
 
-		/// <summary>
-		/// 检查旧密码和输入的密码是否一致
-		/// </summary>
-		/// <param name="accountId"></param>
-		/// <param name="param"></param>
-		/// <returns></returns>
-		public ActionResult CheckPassword(Int32 accountId, String param)
-		{
-			#region 参数验证
+        /// <summary>
+        /// 检查旧密码和输入的密码是否一致
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult CheckPassword(Int32 accountId, String param)
+        {
+            #region 参数验证
 
-			Parameter.Validate(accountId).Validate(param);
+            Parameter.Validate(accountId).Validate(param);
 
-			#endregion
+            #endregion
 
-			var response = new ResponseModel<dynamic>();
-			var result = _accountServices.CheckPassword(accountId, param);
-			response.IsSuccess = true;
-			response.Model = result ? new { status = "y", info = "" } : new { status = "n", info = "原始密码错误" };
+            var response = new ResponseModel<dynamic>();
+            var result = _accountServices.CheckPassword(accountId, param);
+            response.IsSuccess = true;
+            response.Model = result ? new { status = "y", info = "" } : new { status = "n", info = "原始密码错误" };
 
-			return Json(response);
-		}
-	}
+            return Json(response);
+        }
+    }
 }
--- NewCRM/NewCRM.Web/Controllers/AppManagerController.cs
+++ NewCRM/NewCRM.Web/Controllers/AppManagerController.cs
@@ -9,152 +9,143 @@ using NewCRM.Web.Controllers.ControllerHelper;
 
 namespace NewCRM.Web.Controllers
 {
-	public class AppManagerController : BaseController
-	{
-		private readonly IAppApplicationServices _appApplicationServices;
+    public class AppManagerController : BaseController
+    {
+        private readonly IAppApplicationServices _appServices;
 
-		public AppManagerController(IAppApplicationServices appApplicationServices)
-		{
-			_appApplicationServices = appApplicationServices;
-		}
+        public AppManagerController(IAppApplicationServices appServices)
+        {
+            _appServices = appServices;
+        }
 
-		#region 页面
+        #region 页面
 
-		/// <summary>
-		/// 首页
-		/// </summary>
-		/// <returns></returns>
-		public ActionResult Index()
-		{
-			ViewData["AppTypes"] = _appApplicationServices.GetAppTypes();
-			ViewData["AppStyles"] = _appApplicationServices.GetAllAppStyles().ToList();
-			ViewData["AppStates"] = _appApplicationServices.GetAllAppStates().Where(w => w.Name == "未审核" || w.Name == "已发布").ToList();
+        /// <summary>
+        /// 首页
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            ViewData["AppTypes"] = _appServices.GetAppTypes();
+            ViewData["AppStyles"] = _appServices.GetAllAppStyles().ToList();
+            ViewData["AppStates"] = _appServices.GetAllAppStates().Where(w => w.Name == "未审核" || w.Name == "已发布").ToList();
 
-			return View();
-		}
+            return View();
+        }
 
-		/// <summary>
-		/// app审核
-		/// </summary>
-		/// <param name="appId"></param>
-		/// <returns></returns>
-		public ActionResult AppAudit(Int32 appId)
-		{
-			AppDto appResult = null;
-			if (appId != 0)// 如果appId为0则是新创建app
-			{
-				appResult = _appApplicationServices.GetApp(appId);
-				ViewData["AppState"] = appResult.AppAuditState;
-			}
+        /// <summary>
+        /// app审核
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult AppAudit(Int32 appId)
+        {
+            AppDto appResult = null;
+            if (appId != 0)// 如果appId为0则是新创建app
+            {
+                appResult = _appServices.GetApp(appId);
+                ViewData["AppState"] = appResult.AppAuditState;
+            }
 
-			ViewData["AppTypes"] = _appApplicationServices.GetAppTypes();
+            ViewData["AppTypes"] = _appServices.GetAppTypes();
 
-			return View(appResult);
-		}
+            return View(appResult);
+        }
 
-		#endregion
+        #endregion
 
-		/// <summary>
-		/// 获取所有的app
-		/// </summary>
-		/// <returns></returns>
-		public ActionResult GetAllApps(Int32 accountId, String searchText, Int32 appTypeId, Int32 appStyleId, String appState, Int32 pageIndex, Int32 pageSize)
-		{
-			var response = new ResponseModels<IList<AppDto>>();
-			Int32 totalCount;
-			var result = _appApplicationServices.GetAccountAllApps(accountId, searchText, appTypeId, appStyleId, appState, pageIndex, pageSize, out totalCount);
-			if (result != null)
-			{
-				response.TotalCount = totalCount;
-				response.IsSuccess = true;
-				response.Message = "获取app列表成功";
-				response.Model = result;
-			}
-			else
-			{
-				response.Message = "获取app列表失败";
-			}
-			return Json(response, JsonRequestBehavior.AllowGet);
-		}
+        /// <summary>
+        /// 获取所有的app
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetAllApps(Int32 accountId, String searchText, Int32 appTypeId, Int32 appStyleId, String appState, Int32 pageIndex, Int32 pageSize)
+        {
+            var response = new ResponseModels<IList<AppDto>>();
+            Int32 totalCount;
+            var result = _appServices.GetAccountAllApps(accountId, searchText, appTypeId, appStyleId, appState, pageIndex, pageSize, out totalCount);
+            if (result != null)
+            {
+                response.TotalCount = totalCount;
+                response.IsSuccess = true;
+                response.Message = "获取app列表成功";
+                response.Model = result;
+            }
+            else
+            {
+                response.Message = "获取app列表失败";
+            }
+            return Json(response, JsonRequestBehavior.AllowGet);
+        }
 
-		/// <summary>
-		/// app审核通过
-		/// </summary>
-		/// <param name="appId"></param>
-		/// <returns></returns>
-		public ActionResult Pass(Int32 appId)
-		{
-			#region 参数验证	
+        /// <summary>
+        /// app审核通过
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Pass(Int32 appId)
+        {
+            #region 参数验证	
+            Parameter.Validate(appId);
+            #endregion
 
-			Parameter.Validate(appId);
+            var response = new ResponseModel();
+            _appServices.Pass(appId);
+            response.IsSuccess = true;
+            response.Message = "app审核通过";
 
-			#endregion
+            return Json(response);
+        }
 
-			var response = new ResponseModel();
-			_appApplicationServices.Pass(appId);
+        /// <summary>
+        /// app审核不通过
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Deny(Int32 appId)
+        {
+            #region 参数验证	
+            Parameter.Validate(appId);
+            #endregion
 
-			response.IsSuccess = true;
-			response.Message = "app审核通过";
-			return Json(response);
-		}
+            var response = new ResponseModel();
+            _appServices.Deny(appId);
+            response.IsSuccess = true;
+            response.Message = "app审核不通过";
 
-		/// <summary>
-		/// app审核不通过
-		/// </summary>
-		/// <param name="appId"></param>
-		/// <returns></returns>
-		public ActionResult Deny(Int32 appId)
-		{
-			#region 参数验证	
+            return Json(response);
+        }
 
-			Parameter.Validate(appId);
+        /// <summary>
+        /// 设置app为今日推荐
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Recommend(Int32 appId)
+        {
+            #region 参数验证	
+            Parameter.Validate(appId);
+            #endregion
 
-			#endregion
+            var response = new ResponseModel();
+            _appServices.SetTodayRecommandApp(appId);
+            response.IsSuccess = true;
+            response.Message = "设置成功";
 
-			var response = new ResponseModel();
-			_appApplicationServices.Deny(appId);
+            return Json(response);
+        }
 
-			response.IsSuccess = true;
-			response.Message = "app审核不通过";
-			return Json(response);
-		}
+        /// <summary>
+        /// 删除app
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult RemoveApp(Int32 appId)
+        {
+            #region 参数验证	
+            Parameter.Validate(appId);
+            #endregion
 
-		/// <summary>
-		/// 设置app为今日推荐
-		/// </summary>
-		/// <param name="appId"></param>
-		/// <returns></returns>
-		public ActionResult Recommend(Int32 appId)
-		{
-			#region 参数验证	
-			Parameter.Validate(appId);
-			#endregion
+            var response = new ResponseModel();
+            _appServices.RemoveApp(appId);
+            response.IsSuccess = true;
+            response.Message = "删除app成功";
 
-			var response = new ResponseModel();
-			_appApplicationServices.SetTodayRecommandApp(appId);
-
-			response.IsSuccess = true;
-			response.Message = "设置成功";
-			return Json(response);
-		}
-
-		/// <summary>
-		/// 删除app
-		/// </summary>
-		/// <param name="appId"></param>
-		/// <returns></returns>
-		public ActionResult RemoveApp(Int32 appId)
-		{
-			#region 参数验证	
-			Parameter.Validate(appId);
-			#endregion
-
-			var response = new ResponseModel();
-			_appApplicationServices.RemoveApp(appId);
-
-			response.IsSuccess = true;
-			response.Message = "删除app成功";
-			return Json(response);
-		}
-	}
+            return Json(response);
+        }
+    }
 }
