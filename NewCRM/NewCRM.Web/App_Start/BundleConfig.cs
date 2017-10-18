e950ce6b5340bd966b08cc2fd9c678eee6cb9027
haoxiaofan <569192072@qq.com>
2017-10-18 21:08:51 +0800

    刪除多餘的js文件

--- NewCRM/NewCRM.Web/App_Start/BundleConfig.cs
+++ NewCRM/NewCRM.Web/App_Start/BundleConfig.cs
@@ -19,8 +19,7 @@ namespace NewCRM.Web
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
