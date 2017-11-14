0c6fc89a9654abc8a6cf0cd469becd24acf661b3
haoxiaofan <569192072@qq.com>
2017-11-14 23:33:27 +0800

    提交更新

--- NewCRM/NewCRM.Infrastructure/CommonTools/ProfileModel.cs
+++ NewCRM/NewCRM.Infrastructure/CommonTools/ProfileModel.cs
@@ -1,5 +1,6 @@
 ﻿using NewCRM.Infrastructure.CommonTools.CustomException;
 using System;
+using System.Configuration;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -8,9 +9,6 @@ namespace NewCRM.Infrastructure.CommonTools
 {
     public class ProfileManager
     {
-        [DllImport("kernel32")]
-        private static extern int GetPrivateProfileString(string sectionName, string key, string defaultValue, byte[] returnBuffer, int size, string filePath);
-
         private static String _profilePath = "";
         public static String GetFileUrl { get; private set; }
 
@@ -22,8 +20,8 @@ namespace NewCRM.Infrastructure.CommonTools
                 _profilePath = profilePath;
             }
 
-            WatcherStrat(profilePath, "*.ini");
-            GetFileUrl = GetSectionValue("FileStorage", "FileUrl");
+            WatcherStrat(profilePath, "ConsoleApp1.exe.config");
+            GetFileUrl = ConfigurationManager.AppSettings["FileStorage"];
         }
 
         private static void WatcherStrat(string path, string filter)
@@ -36,21 +34,9 @@ namespace NewCRM.Infrastructure.CommonTools
 
             watcher.Changed += (obj, sender) =>
             {
-                GetFileUrl = GetSectionValue("FileStorage", "FileUrl");
+                GetFileUrl = ConfigurationManager.AppSettings["FileStorage"];
             };
             watcher.EnableRaisingEvents = true;
-        }
-
-        private static String GetSectionValue(String sectionName, String key)
-        {
-            Byte[] buffer = new Byte[2048];
-            Int32 length = GetPrivateProfileString(sectionName, key, "发生错误", buffer, 999, _profilePath);
-            String result = Encoding.Default.GetString(buffer, 0, length);
-            if (!String.IsNullOrEmpty(result))
-            {
-                throw new BusinessException("FileUrl配置参数获取失败");
-            }
-            return result;
         }
     }
 }
