2ab8c87b5f7de0e3203bfecf15bb25599a82ca17
haoxiaofan <569192072@qq.com>
2017-11-14 22:43:00 +0800

    提交更新

--- NewCRM/NewCRM.Infrastructure/CommonTools/ProfileModel.cs
+++ NewCRM/NewCRM.Infrastructure/CommonTools/ProfileModel.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using NewCRM.Infrastructure.CommonTools.CustomException;
+using System;
+using System.IO;
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace NewCRM.Infrastructure.CommonTools
 {
@@ -9,6 +12,7 @@ namespace NewCRM.Infrastructure.CommonTools
         private static extern int GetPrivateProfileString(string sectionName, string key, string defaultValue, byte[] returnBuffer, int size, string filePath);
 
         private static String _profilePath = "";
+        private static String _fileUrl = "";
 
         public static void Init(String profilePath = default(String))
         {
@@ -16,12 +20,37 @@ namespace NewCRM.Infrastructure.CommonTools
             {
                 _profilePath = profilePath;
             }
+
+            WatcherStrat(profilePath, "*.ini");
         }
 
-    }
+        private static void WatcherStrat(string path, string filter)
+        {
+            var watcher = new FileSystemWatcher();
+            watcher.Path = path;
+            watcher.Filter = filter;
 
-    public class ProfileModel
-    {
-        public String FileUrl { get; set; }
+            watcher.Changed += (obj, sender) =>
+            {
+
+            };
+            watcher.EnableRaisingEvents = true;
+        }
+
+        public String GetFileUrl
+        {
+            get
+            {
+                byte[] buffer = new byte[2048];
+                Int32 length = GetPrivateProfileString("FileStorage", "FileUrl", "发生错误", buffer, 999, _profilePath);
+                String result = Encoding.Default.GetString(buffer, 0, length);
+                if (!String.IsNullOrEmpty(result))
+                {
+                    throw new BusinessException("FileUrl配置参数获取失败");
+                }
+
+                return result;
+            }
+        }
     }
 }
