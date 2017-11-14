3f806bd7ef8b5e1769eea76d7a91260f5fbfe94f
haoxiaofan <569192072@qq.com>
2017-11-14 22:17:43 +0800

    提交更新

---
+++ NewCRM/NewCRM.Infrastructure/CommonTools/ProfileModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace NewCRM.Infrastructure.CommonTools
+{
+    public class ProfileModel
+    {
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string sectionName, string key, string defaultValue, byte[] returnBuffer, int size, string filePath);
+
+        private static String _profilePath = "";
+
+        public static void Init(String profilePath = default(String))
+        {
+            if (profilePath != default(String))
+            {
+                _profilePath = profilePath;
+            }
+
+
+        }
+    }
+}
