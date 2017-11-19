3d0b608070d38492a9b6e1cd23982691f7b92a72
haoxiaofan <569192072@qq.com>
2017-11-19 16:32:15 +0800

    提交更新

--- NewCRM.Web/App_Start/BundleConfig.cs
+++ NewCRM.Web/App_Start/BundleConfig.cs
@@ -9,17 +9,17 @@ namespace NewCRM.Web
         {
             //js
             bundles.Add(new ScriptBundle("~/bundles/js").Include(
-                "~/Scripts/jquery-3.2.1.min.js",
-                "~/Scripts/HoorayUI/js/HoorayJs/jquery-browser.js",
-                "~/Scripts/HoorayUI/js/artDialog4.1.7/jquery.artDialog.js",
-                "~/Scripts/HoorayUI/js/artDialog4.1.7/plugins/iframeTools.js",
-                "~/Scripts/HoorayUI/js/HoorayLibs/hooraylibs.js",
-                "~/Scripts/HoorayUI/js/bootstrap/js/bootstrap.min.js",
-                "~/Scripts/HoorayUI/js/sugar/sugar-1.4.1.min.js",
-                "~/Scripts/HoorayUI/js/HoorayJs/hros.min.js",
-                "~/Scripts/HoorayUI/js/validate/Validform_v5.3.2/Validform_v5.3.2_min.js",
-                "~/Scripts/HoorayUI/js/webuploader-0.1.5/webuploader.js"
-                ));
+            "~/Scripts/jquery-3.2.1.min.js",
+            "~/Scripts/HoorayUI/js/HoorayJs/jquery-browser.js",
+            /*"~/Scripts/HoorayUI/js/artDialog4.1.7/jquery.artDialog.source.js",
+            "~/Scripts/HoorayUI/js/artDialog4.1.7/plugins/iframeTools.js",*/
+            "~/Scripts/HoorayUI/js/HoorayLibs/hooraylibs.js",
+            "~/Scripts/HoorayUI/js/bootstrap/js/bootstrap.min.js",
+            "~/Scripts/HoorayUI/js/sugar/sugar-1.4.1.min.js",
+            "~/Scripts/HoorayUI/js/HoorayJs/hros.min.js",
+            "~/Scripts/HoorayUI/js/validate/Validform_v5.3.2/Validform_v5.3.2_min.js",
+            "~/Scripts/HoorayUI/js/webuploader-0.1.5/webuploader.js"
+            ));
 
             // 将 EnableOptimizations 设为 false 以进行调试。有关详细信息，
             // 请访问 http://go.microsoft.com/fwlink/?LinkId=301862
