a1898fac7217467ed0588c2fc9430ddb535a2eba
haoxiaofan <569192072@qq.com>
2017-11-19 13:01:46 +0800

    设置js捆绑压缩

--- NewCRM.Web/App_Start/BundleConfig.cs
+++ NewCRM.Web/App_Start/BundleConfig.cs
@@ -7,21 +7,21 @@ namespace NewCRM.Web
         // 有关绑定的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-            
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-            //            "~/Scripts/jquery.validate*"));
+            //js
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                "~/Scripts/jquery-3.2.1.min.js",
+                "~/Scripts/HoorayUI/js/HoorayJs/jquery-browser.js",
+                "~/Scripts/HoorayUI/js/artDialog4.1.7/jquery.artDialog.js",
+                "~/Scripts/HoorayUI/js/artDialog4.1.7/plugins/iframeTools.js",
+                "~/Scripts/HoorayUI/js/HoorayLibs/hooraylibs.js",
+                "~/Scripts/HoorayUI/js/bootstrap/js/bootstrap.min.js",
+                "~/Scripts/HoorayUI/js/sugar/sugar-1.4.1.min.js",
+                "~/Scripts/HoorayUI/js/HoorayJs/hros.min.js",
+                "~/Scripts/HoorayUI/js/validate/Validform_v5.3.2/Validform_v5.3.2_min.js",
+                "~/Scripts/HoorayUI/js/webuploader-0.1.5/webuploader.js"
+                ));
 
-            // 使用要用于开发和学习的 Modernizr 的开发版本。然后，当你做好
-            // 生产准备时，请使用 http://modernizr.com 上的生成工具来仅选择所需的测试。
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-            //          "~/Scripts/bootstrap.js",
-            //          "~/Scripts/respond.js"));
-
+            //css
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
