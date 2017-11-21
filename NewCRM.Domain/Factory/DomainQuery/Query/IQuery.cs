29a1f1570f4ec82908a3273c1c440d977b5b0ef1
haoxiaofan <569192072@qq.com>
2017-11-21 14:02:25 +0800

    提交更新

--- NewCRM.Domain/Factory/DomainQuery/Query/IQuery.cs
+++ NewCRM.Domain/Factory/DomainQuery/Query/IQuery.cs
@@ -11,32 +11,16 @@ namespace NewCRM.Domain.Factory.DomainQuery.Query
         /// <summary>
         /// 查找并返回单个对象
         /// </summary>
-        T FindOne<T>(Specification<T> specification, Expression<Func<T, Object>> selector) where T : DomainModelBase, IAggregationRoot;
-
-        /// <summary>
-        /// 查找并返回单个对象
-        /// </summary>
-        T FindOne<T>(Specification<T> specification) where T : DomainModelBase, IAggregationRoot;
+        U FindOne<T, U>(Specification<T> specification, Expression<Func<T, U>> selector = default(Expression<Func<T, U>>)) where T : DomainModelBase, IAggregationRoot;
 
         /// <summary>
         /// 查找并返回集合
         /// </summary>
-        IEnumerable<T> Find<T>(Specification<T> specification, Expression<Func<T, Object>> selector) where T : DomainModelBase, IAggregationRoot;
-
-        /// <summary>
-        /// 查找并返回集合
-        /// </summary>
-        IEnumerable<T> Find<T>(Specification<T> specification) where T : DomainModelBase, IAggregationRoot;
+        IEnumerable<U> Find<T, U>(Specification<T> specification, Expression<Func<T, U>> selector = default(Expression<Func<T, U>>)) where T : DomainModelBase, IAggregationRoot;
 
         /// <summary>
         /// 分页
         /// </summary>
-        IEnumerable<T> PageBy<T>(Specification<T> specification, Int32 pageIndex, Int32 pageSize, out Int32 totalCount, Expression<Func<T, Object>> selector ) where T : DomainModelBase, IAggregationRoot;
-
-        /// <summary>
-        /// 分页
-        /// </summary>
-        IEnumerable<T> PageBy<T>(Specification<T> specification, Int32 pageIndex, Int32 pageSize, out Int32 totalCount) where T : DomainModelBase, IAggregationRoot;
-
+        IEnumerable<U> PageBy<T, U>(Specification<T> specification, Int32 pageIndex, Int32 pageSize, out Int32 totalCount, Expression<Func<T, U>> selector = default(Expression<Func<T, U>>)) where T : DomainModelBase, IAggregationRoot;
     }
 }
