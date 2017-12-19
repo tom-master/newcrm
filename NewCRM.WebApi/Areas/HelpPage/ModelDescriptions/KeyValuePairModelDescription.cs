686f25dc5793ceb0a892c03dd7de5335e8214080
haoxiaofan <569192072@qq.com>
2017-12-19 21:30:51 +0800

    初始化webapi项目

---
+++ NewCRM.WebApi/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace NewCRM.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ NewCRM.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NewCRM.WebApi.WebApiApplication" Language="C#" %>
