0ae6d51de0aa7fabff97bf8532c6c33babc16a23
haoxiaofan <569192072@qq.com>
2018-01-14 19:35:08 +0800

    commit modify

--- NewCRM.Domain/BaseServiceContext.cs
+++ NewCRM.Domain/BaseServiceContext.cs
@@ -29,19 +29,6 @@ namespace NewCRM.Domain
             return dbResult;
         }
 
-        protected IList<TModel> GetCache<TModel>(String cacheKey, Func<IList<TModel>> func)
-        {
-            var cacheResult = _cacheQuery.StringGet<IList<TModel>>(cacheKey);
-            if (cacheResult != null)
-            {
-                return cacheResult;
-            }
-
-            var dbResult = func();
-            _cacheQuery.StringSet(cacheKey, dbResult);
-            return dbResult;
-        }
-
         /// <summary>
         /// 更新时移除旧的缓存键
         /// </summary>
