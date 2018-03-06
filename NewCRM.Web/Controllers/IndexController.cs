e8887423ed30adcce6ac3becfcfc8782caabd751
haoxiaofan <569192072@qq.com>
2018-03-06 22:24:07 +0800

    commit modify

--- NewCRM.Web/Controllers/IndexController.cs
+++ NewCRM.Web/Controllers/IndexController.cs
@@ -160,7 +160,7 @@ namespace NewCRM.Web.Controllers
         /// <summary>
         /// 创建一个窗口
         /// </summary>
-        [HttpGet]
+        [HttpPost]
         public ActionResult CreateWindow(Int32 id, String type)
         {
 
