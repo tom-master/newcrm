5cab3df7a62450e67398f6292e0ef4aace4b4adc
haoxiaofan <569192072@qq.com>
2018-03-09 15:17:12 +0800

    commit modify

--- NewCRM.DomainService/Interface/IAppContext.cs
+++ NewCRM.DomainService/Interface/IAppContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using NewCRM.Domain.Entitys.System;
 using NewCRM.Dto;
 
@@ -20,104 +21,103 @@ namespace NewCRM.Domain.Services.Interface
         /// <summary>
         /// 获取app
         /// </summary>
-        App GetApp(Int32 appId);
+        Task<App> GetAppAsync(Int32 appId);
 
         /// <summary>
         /// 是否已经安装app
         /// </summary>
-        Boolean IsInstallApp(Int32 accountId, Int32 appId);
+        Task<Boolean> IsInstallAppAsync(Int32 accountId, Int32 appId);
 
         /// <summary>
         /// 获取系统App
         /// </summary>
-        List<App> GetSystemApp(IEnumerable<Int32> appIds = default(IEnumerable<Int32>));
+        Task<List<App>> GetSystemAppAsync(IEnumerable<Int32> appIds = default(IEnumerable<Int32>));
 
 
         /// <summary>
         /// 获取当前账户下已开发和未发布的app
         /// </summary>
-        Tuple<Int32, Int32> GetAccountDevelopAppCountAndNotReleaseAppCount(Int32 accountId);
+        Task<Tuple<Int32, Int32>> GetAccountDevelopAppCountAndNotReleaseAppCountAsync(Int32 accountId);
 
         /// <summary>
         /// 获取所有App类型
         /// </summary>
         /// <returns></returns>
-        List<AppType> GetAppTypes();
+        Task<List<AppType>> GetAppTypesAsync();
 
         /// <summary>
         /// 获取今日推荐
         /// </summary>
-        TodayRecommendAppDto GetTodayRecommend(Int32 accountId);
+        Task<TodayRecommendAppDto> GetTodayRecommendAsync(Int32 accountId);
 
         /// <summary>
         /// 检查应用类型名称
         /// </summary>
-        Boolean CheckAppTypeName(String appTypeName);
+        Task<Boolean> CheckAppTypeNameAsync(String appTypeName);
 
         /// <summary>
         /// 更改app评分
         /// </summary>
-        void ModifyAppStar(Int32 accountId, Int32 appId, Int32 starCount);
+        Task ModifyAppStarAsync(Int32 accountId, Int32 appId, Int32 starCount);
 
         /// <summary>
         /// 创建新的app
         /// </summary>
-        void CreateNewApp(App app);
+        Task CreateNewAppAsync(App app);
 
         /// <summary>
         /// app审核通过
         /// </summary>
-        void Pass(Int32 appId);
+        Task PassAsync(Int32 appId);
 
         /// <summary>
         /// app审核不通过
         /// </summary>
-        void Deny(Int32 appId);
+        Task DenyAsync(Int32 appId);
 
         /// <summary>
         /// 设置今日推荐app
         /// </summary>
-        void SetTodayRecommandApp(Int32 appId);
+        Task SetTodayRecommandAppAsync(Int32 appId);
 
         /// <summary>
         /// 移除app
         /// </summary>
-        void RemoveApp(Int32 appId);
+        Task RemoveAppAsync(Int32 appId);
 
         /// <summary>
         /// 发布app
         /// </summary>
-        void ReleaseApp(Int32 appId);
+        Task ReleaseAppAsync(Int32 appId);
 
         /// <summary>
         /// 修改开发者（用户）的app信息
         /// </summary>
-        void ModifyAccountAppInfo(Int32 accountId, App app);
+        Task ModifyAccountAppInfoAsync(Int32 accountId, App app);
 
         /// <summary>
         /// 删除App分类
         /// </summary>
-        void DeleteAppType(Int32 appTypeId);
+        Task DeleteAppTypeAsync(Int32 appTypeId);
 
         /// <summary>
         /// 创建新的app分类
         /// </summary>
-        void CreateNewAppType(AppType appType);
+        Task CreateNewAppTypeAsync(AppType appType);
 
         /// <summary>
         /// 修改app分类
         /// </summary>
-        void ModifyAppType(String appTypeName, Int32 appTypeId);
+        Task ModifyAppTypeAsync(String appTypeName, Int32 appTypeId);
 
         /// <summary>
         /// 更改app图标
         /// </summary>
-        void ModifyAppIcon(Int32 accountId, Int32 appId, String newIcon);
+        Task ModifyAppIconAsync(Int32 accountId, Int32 appId, String newIcon);
 
         /// <summary>
         /// 用户安装app
         /// </summary> 
-        void Install(Int32 accountId, Int32 appId, Int32 deskNum);
-
+        Task InstallAsync(Int32 accountId, Int32 appId, Int32 deskNum);
     }
 }
