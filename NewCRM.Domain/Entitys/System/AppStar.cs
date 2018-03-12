ec9a7b20f708e0854122a268fa01551e0a45253a
haoxiaofan <569192072@qq.com>
2018-03-12 22:01:32 +0800

    commit modify

--- NewCRM.Domain/Entitys/System/AppStar.cs
+++ NewCRM.Domain/Entitys/System/AppStar.cs
@@ -2,32 +2,71 @@
 
 namespace NewCRM.Domain.Entitys.System
 {
-    public partial class AppStar : DomainModelBase
-    {
-        /// <summary>
-        /// 账号
-        /// </summary>
-        public Int32 AccountId { get; private set; }
+	public partial class AppStar: DomainModelBase
+	{
+		private Int32 _accountId;
 
-        /// <summary>
-        /// 应用Id
-        /// </summary>
-        public Int32 AppId { get; private set; }
+		public Int32 _appId;
 
-        /// <summary>
-        /// 评分
-        /// </summary>
-        public Double StartNum { get; private set; }
+		public Double _startNum;
 
 
-        public AppStar(Int32 accountId, Double startNum)
-        {
-            AccountId = accountId;
-            StartNum = startNum;
-        }
+		/// <summary>
+		/// 账号
+		/// </summary>
+		public Int32 AccountId
+		{
+			get { return _accountId; }
+			private set
+			{
+				if (_accountId != value)
+				{
+					_accountId = value;
+					OnPropertyChanged("AccountId");
+				}
+			}
+		}
 
-        public AppStar()
-        {
-        }
-    }
+		/// <summary>
+		/// 应用Id
+		/// </summary>
+		public Int32 AppId
+		{
+			get { return _appId; }
+			private set
+			{
+				if (_appId != value)
+				{
+					_appId = value;
+					OnPropertyChanged("AppId");
+				}
+			}
+		}
+
+		/// <summary>
+		/// 评分
+		/// </summary>
+		public Double StartNum
+		{
+			get { return _startNum; }
+			private set
+			{
+				if (_startNum != value)
+				{
+					_startNum = value;
+					OnPropertyChanged("StartNum");
+				}
+			}
+		}
+
+		public AppStar(Int32 accountId, Int32 appId, Double startNum)
+		{
+			AccountId = accountId;
+			StartNum = startNum;
+		}
+
+		public AppStar()
+		{
+		}
+	}
 }
