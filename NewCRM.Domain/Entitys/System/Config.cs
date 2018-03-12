789f845d9152cc1e53b2290f68e96ed9639a7af1
haoxiaofan <569192072@qq.com>
2018-03-12 22:32:19 +0800

    commit modify

--- NewCRM.Domain/Entitys/System/Config.cs
+++ NewCRM.Domain/Entitys/System/Config.cs
@@ -5,96 +5,271 @@ using System.ComponentModel.DataAnnotations;
 
 namespace NewCRM.Domain.Entitys.System
 {
-    [Description("用户配置"), Serializable]
-    public partial class Config : DomainModelBase
-    {
-        #region public property
+	[Description("用户配置"), Serializable]
+	public partial class Config: DomainModelBase
+	{
+		#region private field
 
-        /// <summary>
-        /// 皮肤
-        /// </summary>
-        [Required]
-        public String Skin { get; private set; }
+		private String _skin;
 
-        /// <summary>
-        /// 用户头像
-        /// </summary>
-        [Required]
-        public String AccountFace { get; private set; }
+		private Int32 _appSize;
 
-        /// <summary>
-        /// app尺寸
-        /// </summary>
-        public Int32 AppSize { get; private set; }
+		private String _accountFace;
 
-        /// <summary>
-        /// app垂直间距
-        /// </summary>
-        public Int32 AppVerticalSpacing { get; private set; }
+		private Int32 _appVerticalSpacing;
 
-        /// <summary>
-        /// app水平间距
-        /// </summary>
-        public Int32 AppHorizontalSpacing { get; private set; }
+		private Int32 _appHorizontalSpacing;
 
-        /// <summary>
-        /// 默认桌面编号
-        /// </summary>
-        public Int32 DefaultDeskNumber { get; private set; }
+		private Int32 _defaultDeskNumber;
 
-        /// <summary>
-        /// 默认桌面数量
-        /// </summary>
-        public Int32 DefaultDeskCount { get; private set; }
+		private Int32 _defaultDeskCount;
 
-        /// <summary>
-        /// 壁纸的展示模式
-        /// </summary>
-        public WallpaperMode WallpaperMode { get; private set; }
+		private WallpaperMode _wallpaperMode;
 
-        /// <summary>
-        /// 壁纸来源
-        /// </summary>
-        public Boolean IsBing { get; private set; }
+		private Boolean _isBing;
 
-        /// <summary>
-        /// app排列方向
-        /// </summary>
-        public AppAlignMode AppXy { get; private set; }
+		private AppAlignMode _appXy;
 
-        /// <summary>
-        /// 码头位置
-        /// </summary>
-        public DockPostion DockPosition { get; private set; }
+		private DockPostion _dockPosition;
 
-        /// <summary>
-        /// 账户Id
-        /// </summary>
-        public Int32 AccountId { get; set; }
+		private Int32 _accountId;
 
-        /// <summary>
-        /// 壁纸Id
-        /// </summary>
-        public Int32 WallpaperId { get; private set; }
+		private Int32 _wallpaperId;
 
-        #endregion
+		#endregion
 
-        #region ctor
 
-        public Config()
-        {
-            AppXy = AppAlignMode.X;
-            DockPosition = DockPostion.Top;
-            AccountFace = @"\Scripts\HoorayUI\img\ui\avatar_48.jpg";
-            Skin = "default";
-            WallpaperMode = WallpaperMode.Fill;
-            AppSize = 48;
-            AppVerticalSpacing = 50;
-            AppHorizontalSpacing = 50;
-            DefaultDeskNumber = 1;
-            DefaultDeskCount = 5;
-        }
+		#region public property
 
-        #endregion
-    }
+		/// <summary>
+		/// 皮肤
+		/// </summary>
+		[Required]
+		public String Skin
+		{
+			get { return _skin; }
+			private set
+			{
+				if (_skin != value)
+				{
+					_skin = value;
+					OnPropertyChanged("Skin");
+				}
+			}
+		}
+
+		/// <summary>
+		/// 用户头像
+		/// </summary>
+		[Required]
+		public String AccountFace
+		{
+			get { return _accountFace; }
+			private set
+			{
+				if (_accountFace != value)
+				{
+					_accountFace = value;
+					OnPropertyChanged("AccountFace");
+				}
+			}
+		}
+
+		/// <summary>
+		/// app尺寸
+		/// </summary>
+		public Int32 AppSize
+		{
+			get { return _appSize; }
+			private set
+			{
+				if (_appSize != value)
+				{
+					_appSize = value;
+					OnPropertyChanged("AppSize");
+				}
+			}
+		}
+
+		/// <summary>
+		/// app垂直间距
+		/// </summary>
+		public Int32 AppVerticalSpacing
+		{
+			get { return _appVerticalSpacing; }
+			private set
+			{
+				if (_appVerticalSpacing != value)
+				{
+					_appVerticalSpacing = value;
+					OnPropertyChanged("AppVerticalSpacing");
+				}
+			}
+		}
+
+		/// <summary>
+		/// app水平间距
+		/// </summary>
+		public Int32 AppHorizontalSpacing
+		{
+			get { return _appHorizontalSpacing; }
+			private set
+			{
+				if (_appHorizontalSpacing != value)
+				{
+					_appHorizontalSpacing = value;
+					OnPropertyChanged("AppHorizontalSpacing");
+				}
+			}
+		}
+
+		/// <summary>
+		/// 默认桌面编号
+		/// </summary>
+		public Int32 DefaultDeskNumber
+		{
+			get { return _defaultDeskNumber; }
+			private set
+			{
+				if (_defaultDeskNumber != value)
+				{
+					_defaultDeskNumber = value;
+					OnPropertyChanged("DefaultDeskNumber");
+				}
+			}
+		}
+
+		/// <summary>
+		/// 默认桌面数量
+		/// </summary>
+		public Int32 DefaultDeskCount
+		{
+			get { return _defaultDeskCount; }
+			private set
+			{
+				if (_defaultDeskCount != value)
+				{
+					_defaultDeskCount = value;
+					OnPropertyChanged("DefaultDeskCount");
+				}
+			}
+		}
+
+		/// <summary>
+		/// 壁纸的展示模式
+		/// </summary>
+		public WallpaperMode WallpaperMode
+		{
+			get { return _wallpaperMode; }
+			private set
+			{
+				if (_wallpaperMode != value)
+				{
+					_wallpaperMode = value;
+					OnPropertyChanged("WallpaperMode");
+				}
+			}
+		}
+
+		/// <summary>
+		/// 壁纸来源
+		/// </summary>
+		public Boolean IsBing
+		{
+			get { return _isBing; }
+			private set
+			{
+				if (_isBing != value)
+				{
+					_isBing = value;
+					OnPropertyChanged("IsBing");
+				}
+			}
+		}
+
+
+		/// <summary>
+		/// app排列方向
+		/// </summary>
+		public AppAlignMode AppXy
+		{
+			get { return _appXy; }
+			private set
+			{
+				if (_appXy != value)
+				{
+					_appXy = value;
+					OnPropertyChanged("AppXy");
+				}
+			}
+		}
+
+		/// <summary>
+		/// 码头位置
+		/// </summary>
+		public DockPostion DockPosition
+		{
+			get { return _dockPosition; }
+			private set
+			{
+				if (_dockPosition != value)
+				{
+					_dockPosition = value;
+					OnPropertyChanged("DockPosition");
+				}
+			}
+		}
+
+		/// <summary>
+		/// 账户Id
+		/// </summary>
+		public Int32 AccountId
+		{
+			get { return _accountId; }
+			private set
+			{
+				if (_accountId != value)
+				{
+					_accountId = value;
+					OnPropertyChanged("AccountId");
+				}
+			}
+		}
+
+		/// <summary>
+		/// 壁纸Id
+		/// </summary>
+		public Int32 WallpaperId
+		{
+			get { return _wallpaperId; }
+			private set
+			{
+				if (_wallpaperId != value)
+				{
+					_wallpaperId = value;
+					OnPropertyChanged("WallpaperId");
+				}
+			}
+		}
+
+		#endregion
+
+		#region ctor
+
+		public Config()
+		{
+			AppXy = AppAlignMode.X;
+			DockPosition = DockPostion.Top;
+			AccountFace = @"\Scripts\HoorayUI\img\ui\avatar_48.jpg";
+			Skin = "default";
+			WallpaperMode = WallpaperMode.Fill;
+			AppSize = 48;
+			AppVerticalSpacing = 50;
+			AppHorizontalSpacing = 50;
+			DefaultDeskNumber = 1;
+			DefaultDeskCount = 5;
+		}
+
+		#endregion
+	}
 }
--- NewCRM.Domain/Entitys/System/Log.cs
+++ NewCRM.Domain/Entitys/System/Log.cs
@@ -8,41 +8,122 @@ namespace NewCRM.Domain.Entitys.System
 	[Description("日志"), Serializable]
 	public class Log: DomainModelBase
 	{
+		#region private field
+
+		private LogLevel _logLevelEnum;
+
+		private String _controller;
+
+		private String _action;
+
+		private String _exceptionMessage;
+
+		private String _track;
+
+		private Int32 _accountId;
+		#endregion
+
 		#region public property
 
 		/// <summary>
 		/// 日志等级
 		/// </summary>
-		public LogLevel LogLevelEnum { get; private set; }
+		public LogLevel LogLevelEnum
+		{
+			get { return _logLevelEnum; }
+			private set
+			{
+				if (_logLevelEnum != value)
+				{
+					_logLevelEnum = value;
+					OnPropertyChanged("LogLevelEnum");
+				}
+			}
+		}
 
 		/// <summary>
 		/// 类名
 		/// </summary>
 		[Required]
-		public String Controller { get; private set; }
+		public String Controller
+		{
+			get { return _controller; }
+			private set
+			{
+				if (_controller != value)
+				{
+					_controller = value;
+					OnPropertyChanged("Controller");
+				}
+			}
+		}
 
 		/// <summary>
 		/// 方法名
 		/// </summary>
 		[Required]
-		public String Action { get; private set; }
+		public String Action
+		{
+			get { return _action; }
+			private set
+			{
+				if (_action != value)
+				{
+					_action = value;
+					OnPropertyChanged("Action");
+				}
+			}
+		}
 
 		/// <summary>
 		/// 异常信息
 		/// </summary>
 		[Required]
-		public String ExceptionMessage { get; private set; }
+		public String ExceptionMessage
+		{
+			get { return _exceptionMessage; }
+			private set
+			{
+				if (_exceptionMessage != value)
+				{
+					_exceptionMessage = value;
+					OnPropertyChanged("ExceptionMessage");
+				}
+			}
+		}
 
 		/// <summary>
 		/// 异常堆栈
 		/// </summary>
 		[Required]
-		public String Track { get; private set; }
+		public String Track
+		{
+			get { return _track; }
+			private set
+			{
+				if (_track != value)
+				{
+					_track = value;
+					OnPropertyChanged("Track");
+				}
+			}
+		}
 
 		/// <summary>
 		/// 用户id
 		/// </summary>
-		public Int32 AccountId { get; private set; }
+		public Int32 AccountId
+		{
+			get { return _accountId; }
+			private set
+			{
+				if (_accountId != value)
+				{
+					_accountId = value;
+					OnPropertyChanged("AccountId");
+				}
+			}
+		}
 		#endregion
 
 		#region ctor
