c9d7456a9aa7e41b322feeac3536f77aa3ff5f45
haoxiaofan <569192072@qq.com>
2018-03-12 22:04:06 +0800

    commit modify

--- NewCRM.Domain/Entitys/System/AppType.cs
+++ NewCRM.Domain/Entitys/System/AppType.cs
@@ -5,40 +5,70 @@ using System.ComponentModel.DataAnnotations;
 
 namespace NewCRM.Domain.Entitys.System
 {
-    [Description("应用类型"), Serializable]
-    public partial class AppType : DomainModelBase
-    {
-        #region public proptery
+	[Description("应用类型"), Serializable]
+	public partial class AppType: DomainModelBase
+	{
+		#region private field
 
-        /// <summary>
-        /// 名称
-        /// </summary>
-        [Required, MaxLength(6)]
-        public String Name { get; private set; }
+		private String _name;
 
-        /// <summary>
-        /// 备注
-        /// </summary>
-        [MaxLength(50)]
-        public String Remark { get; private set; }
+		private String _remark;
+		#endregion
 
 
-        #endregion
+		#region public proptery
 
-        #region ctor
-        /// <summary>
-        /// 实例化一个app类型对象
-        /// </summary>
-        public AppType(String name, String remark = default(String))
-        {
-            Name = name;
-            Remark = remark;
-        }
+		/// <summary>
+		/// 名称
+		/// </summary>
+		[Required, MaxLength(6)]
+		public String Name
+		{
+			get { return _name; }
+			private set
+			{
+				if (_name != value)
+				{
+					_name = value;
+					OnPropertyChanged("Name");
+				}
+			}
+		}
 
-        public AppType()
-        {
-        }
+		/// <summary>
+		/// 备注
+		/// </summary>
+		[MaxLength(50)]
+		public String Remark
+		{
+			get { return _remark; }
+			private set
+			{
+				if (_remark != value)
+				{
+					_remark = value;
+					OnPropertyChanged("Remark");
+				}
+			}
+		}
 
-        #endregion
-    }
+
+		#endregion
+
+		#region ctor
+		/// <summary>
+		/// 实例化一个app类型对象
+		/// </summary>
+		public AppType(String name, String remark = default(String))
+		{
+			Name = name;
+			Remark = remark;
+		}
+
+		public AppType()
+		{
+		}
+
+		#endregion
+	}
 }
