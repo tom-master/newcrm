72a03d58e642011d6b353208e24d06da498af96f
haoxiaofan <569192072@qq.com>
2018-03-12 17:29:13 +0800

    commit modify

--- NewCRM.Domain/Entitys/Security/Role.cs
+++ NewCRM.Domain/Entitys/Security/Role.cs
@@ -8,29 +8,105 @@ namespace NewCRM.Domain.Entitys.Security
     [Description("角色"), Serializable]
     public partial class Role : DomainModelBase
     {
+        #region private field
+        /// <summary>
+        /// 名称
+        /// </summary>
+        private String _name;
+
+        /// <summary>
+        /// 角色标识
+        /// </summary>
+        private String _roleIdentity;
+
+        /// <summary>
+        /// 备注
+        /// </summary>
+        private String _remark;
+
+        private Boolean _isAllowDisable;
+
+        private IList<RolePower> _powers;
+        #endregion
+
         #region public property
 
         /// <summary>
         /// 名称
         /// </summary>
         [Required, MaxLength(6)]
-        public String Name { get; private set; }
+        public String Name
+        {
+            get { return _name; }
+            private set
+            {
+                if(_name == value)
+                {
+                    _name = value;
+                    OnPropertyChanged(Name);
+                }
+            }
+        }
 
         /// <summary>
         /// 角色标识
         /// </summary>
         [Required, MaxLength(20)]
-        public String RoleIdentity { get; set; }
+        public String RoleIdentity
+        {
+            get { return _roleIdentity; }
+            private set
+            {
+                if(_roleIdentity == value)
+                {
+                    _roleIdentity = value;
+                    OnPropertyChanged(RoleIdentity);
+                }
+            }
+        }
 
         /// <summary>
         /// 备注
         /// </summary>
         [MaxLength(50)]
-        public String Remark { get; private set; }
+        public String Remark
+        {
+            get { return _remark; }
+            private set
+            {
+                if(_remark == value)
+                {
+                    _remark = value;
+                    OnPropertyChanged(Remark);
+                }
+            }
+        }
 
-        public Boolean IsAllowDisable { get; set; }
+        public Boolean IsAllowDisable
+        {
+            get { return _isAllowDisable; }
+            private set
+            {
+                if(_isAllowDisable == value)
+                {
+                    _isAllowDisable = value;
+                    OnPropertyChanged("IsAllowDisable");
+                }
+            }
+        }
 
-        public IList<RolePower> Powers { get; private set; }
+        public IList<RolePower> Powers
+        {
+            get { return _powers; }
+            private set
+            {
+                if(_powers == value)
+                {
+                    _powers = value;
+                    OnPropertyChanged("Powers");
+                }
+            }
+        }
 
         #endregion
 
