6b6647183aa22df15479f24049147e3c3c522159
haoxiaofan <569192072@qq.com>
2018-03-14 21:58:31 +0800

    commit modify

--- NewCRM.Domain/Entitys/Agent/AccountRole.cs
+++ NewCRM.Domain/Entitys/Agent/AccountRole.cs
@@ -3,71 +3,71 @@ using System.ComponentModel.DataAnnotations;
 
 namespace NewCRM.Domain.Entitys.Agent
 {
-    public class AccountRole : DomainModelBase
-    {
-        #region private field
-        private Int32 _accountId;
+	public partial class AccountRole: DomainModelBase
+	{
+		#region private field
+		private Int32 _accountId;
 
-        private Int32 _roleId;
-        #endregion
+		private Int32 _roleId;
+		#endregion
 
-        #region public property
-        [Required]
-        public Int32 AccountId
-        {
-            get
-            {
-                return _accountId;
-            }
-            private set
-            {
-                if(_accountId != value)
-                {
-                    _accountId = value;
-                    OnPropertyChanged(nameof(AccountId));
-                }
-            }
-        }
+		#region public property
+		[Required]
+		public Int32 AccountId
+		{
+			get
+			{
+				return _accountId;
+			}
+			private set
+			{
+				if (_accountId != value)
+				{
+					_accountId = value;
+					OnPropertyChanged(nameof(AccountId));
+				}
+			}
+		}
 
-        [Required]
-        public Int32 RoleId
-        {
-            get
-            {
-                return _roleId;
-            }
-            private set
-            {
-                if(_roleId != value)
-                {
-                    _roleId = value;
-                    OnPropertyChanged(nameof(RoleId));
-                }
-            }
-        }
+		[Required]
+		public Int32 RoleId
+		{
+			get
+			{
+				return _roleId;
+			}
+			private set
+			{
+				if (_roleId != value)
+				{
+					_roleId = value;
+					OnPropertyChanged(nameof(RoleId));
+				}
+			}
+		}
 
-        #endregion
+		#endregion
 
-        #region ctor
+		#region ctor
 
-        public AccountRole(Int32 accountId, Int32 roleId)
-        {
-            AccountId = accountId;
-            RoleId = roleId;
-        }
+		public AccountRole(Int32 accountId, Int32 roleId)
+		{
+			AccountId = accountId;
+			RoleId = roleId;
+		}
 
-        public AccountRole() { }
+		public AccountRole() { }
 
-        public void Remove() => IsDeleted = true;
+		public void Remove() => IsDeleted = true;
 
-        #endregion
-    }
+		#endregion
+	}
 
-    /// <summary>
-    /// AccountRoleExtension
-    /// </summary>
-    public partial class AccountRole
-    {
+	/// <summary>
+	/// AccountRoleExtension
+	/// </summary>
+	public partial class AccountRole
+	{
 
-    }
+	}
 }
