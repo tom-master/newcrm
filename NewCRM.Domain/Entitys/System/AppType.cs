3d49fbd1868adcad3fd973588be9097fe8cc90a6
haoxiaofan <569192072@qq.com>
2018-03-14 16:50:56 +0800

    commit modify

--- NewCRM.Domain/Entitys/System/AppType.cs
+++ NewCRM.Domain/Entitys/System/AppType.cs
@@ -5,76 +5,88 @@ using System.ComponentModel.DataAnnotations;
 
 namespace NewCRM.Domain.Entitys.System
 {
-	[Description("应用类型"), Serializable]
-	public partial class AppType: DomainModelBase
-	{
-		#region private field
+    [Description("应用类型"), Serializable]
+    public partial class AppType : DomainModelBase
+    {
+        #region private field
 
-		private String _name;
+        private String _name;
 
-		private String _remark;
-		#endregion
+        private String _remark;
+        #endregion
 
-		#region public proptery
+        #region public proptery
 
-		/// <summary>
-		/// 名称
-		/// </summary>
-		[Required, MaxLength(6)]
-		public String Name
-		{
-			get { return _name; }
-			private set
-			{
-				if (_name != value)
-				{
-					_name = value;
-					OnPropertyChanged(nameof(Name));
-				}
-			}
-		}
+        /// <summary>
+        /// 名称
+        /// </summary>
+        [Required, MaxLength(6)]
+        public String Name
+        {
+            get { return _name; }
+            private set
+            {
+                if(_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
 
-		/// <summary>
-		/// 备注
-		/// </summary>
-		[MaxLength(50)]
-		public String Remark
-		{
-			get { return _remark; }
-			private set
-			{
-				if (_remark != value)
-				{
-					_remark = value;
-					OnPropertyChanged(nameof(Remark));
-				}
-			}
-		}
+        /// <summary>
+        /// 备注
+        /// </summary>
+        [MaxLength(50)]
+        public String Remark
+        {
+            get { return _remark; }
+            private set
+            {
+                if(_remark != value)
+                {
+                    _remark = value;
+                    OnPropertyChanged(nameof(Remark));
+                }
+            }
+        }
 
-		#endregion
+        #endregion
 
-		#region ctor
-		/// <summary>
-		/// 实例化一个app类型对象
-		/// </summary>
-		public AppType(String name, String remark = default(String))
-		{
-			Name = name;
-			Remark = remark;
-		}
+        #region ctor
+        /// <summary>
+        /// 实例化一个app类型对象
+        /// </summary>
+        public AppType(String name, String remark = default(String))
+        {
+            Name = name;
+            Remark = remark;
+        }
 
-		public AppType()
-		{
-		}
+        public AppType()
+        {
+        }
 
-		#endregion
-	}
+        #endregion
+    }
 
     /// <summary>
     /// AppTypeExtension
     /// </summary>
     public partial class AppType
     {
+        public AppType ModifyName(String appTypeName)
+        {
+            Name = appTypeName;
+            OnPropertyChanged(nameof(Name));
+            return this;
+        }
 
+        public AppType ModifyRemark(String remark)
+        {
+            Remark = remark;
+            OnPropertyChanged(nameof(Remark));
+            return this;
+        }
     }
 }
