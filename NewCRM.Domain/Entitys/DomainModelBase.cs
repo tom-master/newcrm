d05b3a7f2b83bbfaaca064fa4bf87cb456f190a5
haoxiaofan <569192072@qq.com>
2018-03-25 14:00:01 +0800

    commit modify

--- NewCRM.Domain/Entitys/DomainModelBase.cs
+++ NewCRM.Domain/Entitys/DomainModelBase.cs
@@ -3,47 +3,49 @@ using System.ComponentModel.DataAnnotations;
 
 namespace NewCRM.Domain.Entitys
 {
-    [Serializable]
-    public abstract class DomainModelBase : PropertyMonitor
-    {
-        #region ctor
+	[Serializable]
+	public abstract class DomainModelBase: PropertyMonitor
+	{
+		#region ctor
 
-        protected DomainModelBase()
-        {
-            IsDeleted = false;
-        }
+		protected DomainModelBase()
+		{
+			IsDeleted = false;
+		}
 
-        #endregion
+		#endregion
 
-        #region public property
+		#region public property
 
-        [Key]
-        public Int32 Id { get; protected set; }
+		[Key]
+		public Int32 Id { get; protected set; }
 
-        public Boolean IsDeleted { get; private set; }
+		public Boolean IsDeleted { get; private set; }
 
-        public DateTime AddTime
-        {
-            get
-            {
-                return DateTime.Now;
-            }
-        }
+		public DateTime AddTime
+		{
+			get
+			{
+				return DateTime.Now;
+			}
+			set { }
+		}
 
-        public DateTime LastModifyTime
-        {
-            get
-            {
-                return DateTime.Now;
-            }
-        }
+		public DateTime LastModifyTime
+		{
+			get
+			{
+				return DateTime.Now;
+			}
+			set { }
+		}
 
-        public void Remove()
-        {
-            IsDeleted = true;
-            OnPropertyChanged(nameof(IsDeleted));
-        }
+		public void Remove()
+		{
+			IsDeleted = true;
+			OnPropertyChanged(nameof(IsDeleted));
+		}
 
-        #endregion
-    }
+		#endregion
+	}
 }
