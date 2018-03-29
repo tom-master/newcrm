6917445c23dac0f7d39527cffe4f022d4754c3f9
haoxiaofan <569192072@qq.com>
2018-03-29 10:22:16 +0800

    commit modify

--- NewCRM.Domain/Entitys/System/AppType.cs
+++ NewCRM.Domain/Entitys/System/AppType.cs
@@ -5,90 +5,90 @@ using System.ComponentModel.DataAnnotations;
 
 namespace NewCRM.Domain.Entitys.System
 {
-    [Description("应用类型"), Serializable]
-    public partial class AppType : DomainModelBase
-    {
-        #region private field
+	[Description("应用类型"), Serializable]
+	public partial class AppType : DomainModelBase
+	{
+		#region private field
 
-        private String _name;
+		private String _name;
 
-        private String _remark;
-        #endregion
+		private String _remark;
+		#endregion
 
-        #region public proptery
+		#region public proptery
 
-        /// <summary>
-        /// 名称
-        /// </summary>
-        [Required, MaxLength(6)]
-        public String Name
-        {
-            get { return _name; }
-            private set
-            {
-                _name = value;
-            }
-        }
+		/// <summary>
+		/// 名称
+		/// </summary>
+		[PropertyRequired, InputRange(1, 6)]
+		public String Name
+		{
+			get { return _name; }
+			private set
+			{
+				_name = value;
+			}
+		}
 
-        /// <summary>
-        /// 备注
-        /// </summary>
-        [MaxLength(50)]
-        public String Remark
-        {
-            get { return _remark; }
-            private set
-            {
-                _remark = value;
-            }
-        }
+		/// <summary>
+		/// 备注
+		/// </summary>
+		[PropertyDefaultValue(typeof(String), ""), InputRange(50)]
+		public String Remark
+		{
+			get { return _remark; }
+			private set
+			{
+				_remark = value;
+			}
+		}
 
-        #endregion
+		#endregion
 
-        #region ctor
-        /// <summary>
-        /// 实例化一个app类型对象
-        /// </summary>
-        public AppType(String name, String remark = default(String))
-        {
-            Name = name;
-            Remark = remark;
-        }
+		#region ctor
+		/// <summary>
+		/// 实例化一个app类型对象
+		/// </summary>
+		public AppType(String name, String remark = default(String))
+		{
+			Name = name;
+			Remark = remark;
+		}
 
-        public AppType()
-        {
-        }
+		public AppType()
+		{
+		}
 
-        #endregion
-    }
+		#endregion
+	}
 
-    /// <summary>
-    /// AppTypeExtension
-    /// </summary>
-    public partial class AppType
-    {
-        public AppType ModifyName(String appTypeName)
-        {
-            if(String.IsNullOrEmpty(appTypeName))
-            {
-                throw new ArgumentException($@"{nameof(appTypeName)} is null");
-            }
+	/// <summary>
+	/// AppTypeExtension
+	/// </summary>
+	public partial class AppType
+	{
+		public AppType ModifyName(String appTypeName)
+		{
+			if(String.IsNullOrEmpty(appTypeName))
+			{
+				throw new ArgumentException($@"{nameof(appTypeName)} is null");
+			}
 
-            Name = appTypeName;
-            OnPropertyChanged(nameof(Name));
-            return this;
-        }
+			Name = appTypeName;
+			OnPropertyChanged(nameof(Name));
+			return this;
+		}
 
-        public AppType ModifyRemark(String remark)
-        {
-            if(String.IsNullOrEmpty(remark))
-            {
-                throw new ArgumentException($@"{nameof(remark)} is null");
-            }
+		public AppType ModifyRemark(String remark)
+		{
+			if(String.IsNullOrEmpty(remark))
+			{
+				throw new ArgumentException($@"{nameof(remark)} is null");
+			}
 
-            Remark = remark;
-            OnPropertyChanged(nameof(Remark));
-            return this;
-        }
-    }
+			Remark = remark;
+			OnPropertyChanged(nameof(Remark));
+			return this;
+		}
+	}
 }
