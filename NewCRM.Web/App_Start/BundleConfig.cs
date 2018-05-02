4b04e3fcc3f59f9dd1ec1d94e04af495efedbd4c
haoxiaofan <569192072@qq.com>
2018-05-02 23:56:33 +0800

    w

--- NewCRM.Web/App_Start/BundleConfig.cs
+++ NewCRM.Web/App_Start/BundleConfig.cs
@@ -9,15 +9,15 @@ namespace NewCRM.Web
         {
             //js
             bundles.Add(new ScriptBundle("~/bundles/js").Include(
-                "~/Scripts/jquery-3.2.1.min.js",
-                "~/Scripts/HoorayJs/jquery-browser.js",
-                "~/Scripts/HoorayLibs/hooraylibs.js",
-                "~/Scripts/bootstrap/js/bootstrap.min.js",
-                "~/Scripts/sugar/sugar-1.4.1.min.js",
-                "~/Scripts/HoorayJs/hros.min.js",
-                "~/Scripts/validate/Validform_v5.3.2/Validform_v5.3.2_min.js",
-                "~/Scripts/webuploader-0.1.5/webuploader.js"
-                ));
+                "wwwroot/js/jquery-3.2.1.min.js",
+				"wwwroot/js/HoorayJs/jquery-browser.js",
+				"wwwroot/js/HoorayLibs/hooraylibs.js",
+				"wwwroot/js/bootstrap/js/bootstrap.min.js",
+				"wwwroot/js/sugar/sugar-1.4.1.min.js",
+				"wwwroot/js/HoorayJs/hros.min.js",
+				"wwwroot/js/validate/Validform_v5.3.2/Validform_v5.3.2_min.js",
+				"wwwroot/js/webuploader-0.1.5/webuploader.js"
+				));
 
             // 将 EnableOptimizations 设为 false 以进行调试。有关详细信息，
             // 请访问 http://go.microsoft.com/fwlink/?LinkId=301862
