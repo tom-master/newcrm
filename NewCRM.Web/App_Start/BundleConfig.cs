9624dcae9773b7a9e802af20ef34640bee26a2c0
haoxiaofan <569192072@qq.com>
2018-05-03 23:36:55 +0800

    w

--- NewCRM.Web/App_Start/BundleConfig.cs
+++ NewCRM.Web/App_Start/BundleConfig.cs
@@ -22,7 +22,7 @@ namespace NewCRM.Web
             // 将 EnableOptimizations 设为 false 以进行调试。有关详细信息，
             // 请访问 http://go.microsoft.com/fwlink/?LinkId=301862
 
-#if !DEBUG
+#if DEBUG
             BundleTable.EnableOptimizations = true;
 #else
             BundleTable.EnableOptimizations = false;
